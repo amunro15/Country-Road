c0595e0056f32feb02d57db69a422193ac16da9d
Amy  Munro <amymunro@Macintosh.local>
2018-01-06 14:32:43 -0500

    edits to accommodate screen sizes

--- js/main.js
+++ js/main.js
@@ -1,41 +1,5 @@
 $(document).ready(function(){
 
-//SLIDESHOW
-  // $(".next").on("click",function(){
-  //   console.log("hi");
-  //   var slideIndex = 1;
-  //   showSlides(slideIndex);
-  //
-  //   function plusSlides(n) {
-  //     console.log(hi);
-  //     showSlides(slideIndex += n);
-  //   }
-  //
-  //   function currentSlide(n) {
-  //     showSlides(slideIndex = n);
-  //   }
-  //
-  //   function showSlides(n) {
-  //     var i;
-  //     var slides = document.getElementsByClassName("mySlides");
-  //     var dots = document.getElementsByClassName("dot");
-  //     if (n > slides.length){
-  //     slideIndex = 1
-  //     }
-  //     if (n < 1){
-  //       slideIndex = slides.length
-  //     }
-  //     for (i = 0; i < slides.length; i++){
-  //       slides[i].style.display = "none";
-  //     }
-  //     for (i = 0; i < dots.length; i++){
-  //       dots[i].className = dots[i].className.replace(" active", "");
-  //     }
-  //     slides[slideIndex-1].style.display = "block";
-  //     dots[slideIndex-1].className += "active";
-  //   }
-  // })
-
   var myArray=["images/jumps.jpg","images/barn1.jpg","images/barn2.jpg","images/barn3.jpg"];
 
   var textArray=["Outdoor Arena","Barn Entrance","Indoor Stalls","Indoor Arena"];
